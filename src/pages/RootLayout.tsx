a1a9bce0d45d828836b1d11a5fe67551f68e84a9
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-31 22:49:00 +1000

    344. Reflecting The Current Navigation State in the UI

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,14 @@
 ﻿import MainNavigation from "../components/MainNavigation.tsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 export default function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/*{navigation.state === "loading" && <p>Loading...</p>}*/}
         <Outlet />
       </main>
     </>
